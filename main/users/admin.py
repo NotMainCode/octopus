cd859e4d7e62f32a3f5bd6304e1f05825212ebf6
aleksandrkomyagin <aleksandrkomyagin8@yandex.ru>
2023-10-19 20:58:07 +0300

    feat: create model User

--- main/users/admin.py
+++ main/users/admin.py
@@ -1,3 +1,11 @@
 """Admin site settings of the 'Users' app."""
-
 from django.contrib import admin
+
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name', 'last_name', 'email',
+    )
