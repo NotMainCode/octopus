7cfc6816fca51ffb025ccb8afc2eff07bc14fb56
NotMainCode <106552134+NotMainCode@users.noreply.github.com>
2023-11-09 04:02:49 +0300

    feat: media settings

--- main/main/settings.py
+++ main/main/settings.py
@@ -103,6 +103,9 @@ USE_TZ = True
 STATIC_URL = "/static/django/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
