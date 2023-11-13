ac17820b0554ec0710610bdbe1041227ffb82111
aleksandrkomyagin <aleksandrkomyagin8@yandex.ru>
2023-11-13 17:39:30 +0300

    feat: views for endpoints me, change_password and change_password_confirm

--- main/api/v1/users/serializers.py
+++ main/api/v1/users/serializers.py
@@ -1,1 +1,78 @@
 """Serializers for the 'users' endpoints of 'Api' application v1."""
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
+
+from rest_framework import serializers
+from rest_framework.exceptions import PermissionDenied
+
+from users.validators import (
+    CustomPasswordValidator,
+    validate_first_name_and_last_name_fields,
+)
+
+User = get_user_model()
+validator = CustomPasswordValidator()
+
+
+class GetUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "first_name", "last_name", "email")
+
+
+class UpdateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "first_name", "last_name", "email")
+        read_only_fields = ("email", "password")
+
+    def validate(self, attrs):
+        for value in attrs.values():
+            validate_first_name_and_last_name_fields(value)
+        return attrs
+
+
+class ChangePasswordConfirmSerializer(serializers.Serializer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = None
+
+    uid = serializers.CharField(write_only=True)
+    token = serializers.CharField(write_only=True)
+    current_password = serializers.CharField(write_only=True, label="Текущий пароль")
+    new_password = serializers.CharField(write_only=True, label="Новый пароль")
+    re_new_password = serializers.CharField(
+        write_only=True, label="Повтор нового пароля"
+    )
+
+    def validate(self, attrs):
+        if attrs["new_password"] != attrs["re_new_password"]:
+            raise serializers.ValidationError("Введены разные пароли.")
+        if attrs["current_password"] == attrs["new_password"]:
+            raise serializers.ValidationError("Текущий пароль и новый совпадают.")
+        uid = attrs["uid"]
+        encode_token = attrs["token"]
+        try:
+            id = int(force_str(urlsafe_base64_decode(uid)))
+            self.user = User.objects.get(pk=id)
+        except (User.DoesNotExist, ValueError, TypeError, OverflowError):
+            raise serializers.ValidationError(
+                {"uid": "Неверный id или пользователь не существует."}
+            )
+        if not default_token_generator.check_token(self.user, encode_token):
+            raise serializers.ValidationError(
+                {"token": "Некорректный токен или срок его действия истёк."}
+            )
+        if not self.user.check_password(attrs["current_password"]):
+            raise serializers.ValidationError(
+                {"current_password": "Неверный текущий пароль."}
+            )
+        if not self.user.is_active:
+            raise PermissionDenied("User is inactive.")
+        return attrs
+
+    def validate_new_password(self, value):
+        validator.validate(value)
+        return value
--- main/api/v1/users/views.py
+++ main/api/v1/users/views.py
@@ -1,1 +1,88 @@
 """Views for 'users' endpoints of 'Api' application v1."""
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode
+
+from rest_framework import serializers, status, views
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from api.v1.users.serializers import (
+    ChangePasswordConfirmSerializer,
+    GetUserSerializer,
+    UpdateUserSerializer,
+)
+
+User = get_user_model()
+
+
+class UserOwnPageView(views.APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        try:
+            user = User.objects.get(email=request.user.email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError({"user": "Пользователь не существует."})
+        serializer = GetUserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request):
+        try:
+            user = User.objects.get(email=request.user.email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError({"user": "Пользователь не существует."})
+        serializer = UpdateUserSerializer(
+            user, data=request.data, context={"request": request}, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserChangePasswordView(views.APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def _generate_url(self, user, request):
+        uid = force_str(urlsafe_base64_encode(force_bytes(user.id)))
+        token = default_token_generator.make_token(user)
+        site = get_current_site(request)
+        protocol = "https:/" if request.is_secure() else "http:/"
+        confirm_url = "/".join(
+            (protocol, site.domain, "#", "change_password_confirm", uid, str(token))
+        )
+        return confirm_url
+
+    def _generate_mail(self, url):
+        mail = {}
+        mail["subject"] = "password changes"
+        mail["message"] = url
+        return mail
+
+    def post(self, request):
+        try:
+            user = User.objects.get(email=request.user.email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError({"user": "Пользователь не существует."})
+        confirm_url = self._generate_url(user, request)
+        mail = self._generate_mail(confirm_url)
+        user.send_mail(user, mail)
+        return Response(
+            status=status.HTTP_204_NO_CONTENT,
+        )
+
+
+class UserChangePasswordConfirmView(views.APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        serializer = ChangePasswordConfirmSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.user
+        user.set_password(serializer.validated_data["new_password"])
+        user.save(update_fields=["password"])
+        return Response(
+            status=status.HTTP_204_NO_CONTENT,
+        )
