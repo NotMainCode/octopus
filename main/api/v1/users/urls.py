1d7b49ea7c1b4600b36a028059b5b89cc1b412f5
aleksandrkomyagin <aleksandrkomyagin8@yandex.ru>
2023-11-15 21:56:31 +0300

    fix: ChangePasswordSerializer and UserChangePasswordView. Remove UpdateUserSerializer

--- main/api/v1/users/urls.py
+++ main/api/v1/users/urls.py
@@ -1,18 +1,9 @@
 """URLs configuration of the 'users' endpoints of 'Api' app v1."""
 from django.urls import path
 
-from api.v1.users.views import (
-    UserChangePasswordConfirmView,
-    UserChangePasswordView,
-    UserOwnPageView,
-)
+from api.v1.users.views import UserChangePasswordView, UserOwnPageView
 
 urlpatterns = [
     path("me/", UserOwnPageView.as_view(), name="me"),
     path("change_password/", UserChangePasswordView.as_view(), name="change_password"),
-    path(
-        "change_password_confirm/",
-        UserChangePasswordConfirmView.as_view(),
-        name="change_password_confirm",
-    ),
 ]
