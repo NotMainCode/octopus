2ec2358a6d44bdeb5bb7b40a99e7630737c22fa3
aleksandrkomyagin <aleksandrkomyagin8@yandex.ru>
2023-11-16 13:30:08 +0300

    feat: refresh token

--- main/api/v1/tokens/urls.py
+++ main/api/v1/tokens/urls.py
@@ -1,3 +1,8 @@
 """URLs configuration of the 'tokens' endpoints of 'Api' app v1."""
+from django.urls import path
 
-urlpatterns = []
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path("refresh/", TokenRefreshView.as_view(), name="refresh_token"),
+]
