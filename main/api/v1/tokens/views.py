1cfc5053af8559e91ac81a15a48083da67c9efbb
NotMainCode <106552134+NotMainCode@users.noreply.github.com>
2023-11-29 02:11:23 +0300

    refactor: drf-spectacular package

--- main/api/v1/tokens/views.py
+++ main/api/v1/tokens/views.py
@@ -1,12 +1,11 @@
 """Views for the endpoints 'tokens' of 'Api' application v1."""
+
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from api.v1.drf_spectacular.custom_decorators import (
-    activate_drf_spectacular_view_decorator,
-)
+from api.v1.drf_spectacular.custom_decorators import get_drf_spectacular_view_decorator
 
 
-@activate_drf_spectacular_view_decorator
+@get_drf_spectacular_view_decorator("tokens")
 class CustomTokenRefreshView(TokenRefreshView):
     """Update tokens.
 
