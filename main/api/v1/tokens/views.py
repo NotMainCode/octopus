77c90483713052bb3a57c7e9e093e23fa3e56c54
NotMainCode <106552134+NotMainCode@users.noreply.github.com>
2023-11-30 18:36:50 +0300

    feat: refresh/ endpoint - check user

--- main/api/v1/tokens/views.py
+++ main/api/v1/tokens/views.py
@@ -1,5 +1,10 @@
 """Views for the endpoints 'tokens' of 'Api' application v1."""
 
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from api.v1.drf_spectacular.custom_decorators import get_drf_spectacular_view_decorator
@@ -7,7 +12,20 @@ from api.v1.drf_spectacular.custom_decorators import get_drf_spectacular_view_de
 
 @get_drf_spectacular_view_decorator("tokens")
 class CustomTokenRefreshView(TokenRefreshView):
-    """Update tokens.
+    """Modified SimpleJWT TokenRefreshView.
 
-    Class is created to generate API documentation.
+    Added checking for user presence in the database and whether the user is active.
     """
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as exc:
+            raise InvalidToken(exc.args[0])
+
+        token = AccessToken(serializer.validated_data["access"])
+        JWTAuthentication().get_user(token)
+
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
