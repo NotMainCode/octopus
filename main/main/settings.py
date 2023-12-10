7d035c45ed7cd8ff99060962820e21bf66c8181e
Stryukov <stryukov.semen@gmail.com>
2023-12-11 10:51:03 +1200

    CSRF are moved to the env file

--- main/main/settings.py
+++ main/main/settings.py
@@ -138,7 +138,7 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 465
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-CSRF_TRUSTED_ORIGINS = ["https://*.octopus-it.ru"]
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "https://*.octopus-it.ru").split(" ")
 
 if DEBUG:
     INSTALLED_APPS.extend(["debug_toolbar", "drf_spectacular"])
