1c2ce96c4e31666956d939b7d1fb51c430807daa
NotMainCode <106552134+NotMainCode@users.noreply.github.com>
2023-12-15 23:53:34 +0300

    refactor: reduce the number of migration files

--- main/api/v1/drf_spectacular/companies/view_decorators.py
+++ main/api/v1/drf_spectacular/companies/view_decorators.py
@@ -3,7 +3,7 @@
 from drf_spectacular.utils import OpenApiResponse, extend_schema, extend_schema_view
 from rest_framework import status
 
-from api.v1.companies.serializers import CompanyDetailSerializer, CompanySerializer
+from api.v1.companies.serializers import CompanyBriefSerializer, CompanySerializer
 from api.v1.drf_spectacular.companies.examples import Response200CompaniesDetailExample
 from api.v1.drf_spectacular.companies.serializers import Response400QueryParamSerializer
 from api.v1.drf_spectacular.serializers import (
@@ -17,7 +17,7 @@ COMPANIES_VIEW_DECORATORS = {
         list=extend_schema(
             tags=("companies",),
             responses={
-                status.HTTP_200_OK: CompanySerializer,
+                status.HTTP_200_OK: CompanyBriefSerializer,
                 status.HTTP_400_BAD_REQUEST: Response400QueryParamSerializer,
                 status.HTTP_404_NOT_FOUND: Response404Serializer,
             },
@@ -25,7 +25,7 @@ COMPANIES_VIEW_DECORATORS = {
         retrieve=extend_schema(
             tags=("companies",),
             responses={
-                status.HTTP_200_OK: CompanyDetailSerializer,
+                status.HTTP_200_OK: CompanySerializer,
                 status.HTTP_404_NOT_FOUND: Response404Serializer,
             },
             examples=[
