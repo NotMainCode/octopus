8e972602de16bf784156e183bef0a9022680013f
NotMainCode <106552134+NotMainCode@users.noreply.github.com>
2023-12-16 18:38:55 +0300

    refactor: users/validators.py

--- main/users/validators.py
+++ main/users/validators.py
@@ -23,50 +23,55 @@ class CustomPasswordValidator:
 
     def validate(self, password, user=None):
         """Validate user password."""
-        if not password:
-            return
         if len(password) < self.min_length:
             raise ValidationError(
-                _("Минимальная длина пароля %(min_length)d символов!"),
-                code="Короткий пароль!",
-                params={"min_length": self.min_length},
+                "Минимальная длина пароля {min_length} символов!".format(
+                    min_length=self.min_length
+                )
             )
-
         if len(password) > self.max_length:
             raise ValidationError(
-                _("Максимальная длина пароля %(max_length)d символов!"),
-                code="Длинный пароль!",
-                params={"max_length": self.max_length},
+                "Максимальная длина пароля {max_length} символов!".format(
+                    max_length=self.max_length
+                )
             )
+        if password.isdigit():
+            raise ValidationError("Пароль не должен содержать только цифры!")
 
-        if re.search(r"[^0-9]", password) is None:
-            raise ValidationError(_("Пароль не должен содержать только цифры!"))
-        pattern = r"[a-zA-Zа-яА-Я-+_.!?@#$%^&*\d+=/]"
-        symbol = set(password) - set("".join(re.findall(pattern, password)))
-        if symbol:
-            raise ValidationError(_(f"Символы <{''.join(symbol)}> запрещены!"))
+        unacceptable_simbols = "".join(
+            set(re.sub(r"^[a-zA-Zа-яА-ЯёЁ+-_.!?@%#№$^&*\d+=/]$", "", password))
+        )
+        if unacceptable_simbols:
+            raise ValidationError(
+                "Символы <{unacceptable_simbols}> запрещены!".format(
+                    unacceptable_simbols=unacceptable_simbols
+                )
+            )
 
     def get_help_text(self):
         """Get help text for the password field."""
-        return _(
+        return (
             f"Ваш пароль должен содержать не менее {self.min_length} и "
             f"не более {self.max_length} символов. "
-            f"Разрешено использовать латинский алфавит, "
-            f"цифры и спецсимволы <-+_.!?@#$%^&*>"
+            f"Разрешено использовать кириллицу, латиницу, "
+            f"цифры и спецсимволы <-+_.!?@#№$%^&*>"
         )
 
 
-def validate_first_name_and_last_name_fields(input_string):
+def validate_first_name_and_last_name_fields(value):
     """Validate the user's first and last name."""
-    if re.search(r"[\d]", input_string) is not None:
+    if re.search(r"[\d]", value) is not None:
         raise ValidationError("Нельзя использовать цифры!")
-    if input_string.count("-") > 1 or input_string.count(" ") > 1:
+    if value.count("-") > 1 or value.count(" ") > 1:
         raise ValidationError("Нельзя использовать 2 знака <-> или два пробела!")
-    symbol = set(input_string) - set(
-        "".join(re.findall(r"[a-zA-Zа-яА-Я- ]", input_string))
-    )
-    if symbol:
-        raise ValidationError("Нельзя использовать эти символы <{}>".format(*symbol))
+
+    unacceptable_simbols = "".join(set(re.sub(r"^[a-zA-Zа-яА-Я- ]$", "", value)))
+    if unacceptable_simbols:
+        raise ValidationError(
+            "Нельзя использовать эти символы <{unacceptable_simbols}>".format(
+                unacceptable_simbols=unacceptable_simbols
+            )
+        )
 
 
 @deconstructible
@@ -112,9 +117,9 @@ class CustomEmailValidator:
         if not self.user_regex.match(user_part):
             raise ValidationError(self.message, code=self.code, params={"value": value})
 
-        if domain_part not in self.domain_allowlist and not self.validate_domain_part(
-            domain_part
-        ):
+        is_domain_part_valid = self.validate_domain_part(domain_part)
+        is_domain_part_in_domain_allowlist = domain_part in self.domain_allowlist
+        if not is_domain_part_in_domain_allowlist and not is_domain_part_valid:
             raise ValidationError(self.message, code=self.code, params={"value": value})
 
     def validate_domain_part(self, domain_part):
