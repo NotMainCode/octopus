4270e7ca2ea26715f5f80014b2c3f295cd659e20
NotMainCode <106552134+NotMainCode@users.noreply.github.com>
2023-12-26 19:42:22 +0300

    refactor: nginx and compose files

--- octopus/octopus/settings.py
+++ octopus/octopus/settings.py
@@ -89,7 +89,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "api_doc/")]
 
 MEDIA_URL = "media/"
