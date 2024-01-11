5151d725dbef900c38032b77495791f726f935f9
NotMainCode <106552134+NotMainCode@users.noreply.github.com>
2024-01-12 00:33:00 +0300

    refactor: move api_doc folder to static folder

--- octopus/octopus/settings.py
+++ octopus/octopus/settings.py
@@ -90,7 +90,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "api_doc/")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/api_doc/")]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
